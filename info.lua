0fc1d2499f2f0ddf26896e0a349f90506fb3357e
Caitlyn Sullivan <caitlyn.sullivan@qsc.com>
2021-04-27 12:02:25 -0600

    updated header per Amanda's comments

--- info.lua
+++ info.lua
@@ -2,7 +2,7 @@ PluginInfo = {
   Name = "Base Plugin",
   Version = "0.0",
   BuildVersion = "0.0.0.0",
-  Id = "edd65ebc-5254-4a7c-afb8-ad4357611785",
-  Author = "QSC",
+  Id = "<guid>",
+  Author = "Your_Company",
   Description = "A very basic plugin"  
 }
